56220f7f8d6f6c5a93deb2c14b4f83d236ad8882
Subin Chaliyath <subinchaliyath@gmail.com>
2021-01-23 14:01:47 +0530

    setting queryParams in routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-examples';
+  name;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+      
+      this.name = params['name'];
+    });
+  }
+  
 }
